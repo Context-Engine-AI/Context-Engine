f38ccdde059ec4eddda863dfb33f61b885ffa91c
Reese <reesevader@hotmail.co.uk>
2025-12-08 17:52:21 +0000

    ctxce cli - rename to mcp bridge

--- ctxce-cli/bin/ctxce.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { runCli } from "../src/cli.js";
-
-runCli().catch((err) => {
-  console.error("[ctxce] Fatal error:", err && err.stack ? err.stack : err);
-  process.exit(1);
-});
